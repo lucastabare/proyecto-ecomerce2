fc00d89911bcf47b1ca5857308316fae1d665025
Lucas Tabare <lucas.tabare@gmail.com>
2021-11-07 13:39:59 -0300

    Terminando El NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,39 +1,43 @@
 import React from "react";
-import { makeStyles } from "@mui/material";
-import AppBar from "@material-ui/core/AppBar"
-import ToolBar from "@material-ui/core/Toolbar"
-import { Typography } from "@mui/material/Typography";
-import { IconButton } from "@mui/material/IconButton";
-import logo from "../../assets/logo.png"
-import {Badge, Button} from "@material-ui/core"
-import { ShoppingCart } from "@mui/icons-material"; 
-import { useStateValue } from "../StateProvider";
-import {Link, useHistory} from "react-router-dom"
-import {auth} from "../firebase"
-import {actionTypes} from "../reducer"
-
+import { makeStyles } from "@mui/styles";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import logo from "../../assets/logo.png";
+import { Button } from "@mui/material";
+import { Badge } from "@material-ui/core";
+import { ShoppingCart } from "@mui/icons-material";
+//import { useStateValue } from "../StateProvider";
+//import { Link, useHistory } from "react-router-dom";
+//import { auth } from "../firebase";
+//import { actionTypes } from "../reducer";
 
 const useStyles = makeStyles((theme) => ({
-    root:{
-        flexGrow: 1,
-        marginBottom: "7rem",
-    },
-    appBar: {
-        backgroundColor: "whitesmoke",
-        bocShadow: "none"
-    },
-    grow:{
-        flexGrow: 1,
-    },
-    button:{
-        marginLeft: theme.spacing(2)
-    },
-    image:{
-        marginRight: "10px"
-    },
-})),
+  root: {
+    flexGrow: 1,
+    marginBottom: "7rem",
+    paddingBottom: "3rem",
+    backgroundColor: "#7fc9a4",
+  },
+  appBar: {
+    backgroundColor: "#7fc9a4",
+    boxShadow: "none",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  button: {
+    marginLeft: theme.spacing,
+    padding: "1rem",
+  },
+  image: {
+    marginRight: "10px",
+    borderRadius: "5px",
+  },
+}));
 
-const NavBar =()=>{
+/* export default function NavBar (){
     const classes = useStyles()
     const [{basket, user}, dispatch] = useStateValue()
     const history = useHistory()
@@ -80,7 +84,45 @@ const NavBar =()=>{
                 </ToolBar>
             </AppBar>
         </div>
-    )
+    )*/
+
+function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <ToolBar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="80px"
+              className={classes.image}
+            />
+          </IconButton>
+          <div className={classes.grow} />
+          <Typography variant="h6" color="textPrimary" component="p">
+            Hola User
+          </Typography>
+          <div className={classes.button}>
+            <Button variant="outlined">
+              <strong>Iniciar Sesion</strong>
+            </Button>
+            <IconButton aria-label="show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart fontSize="large" color="primary" />
+              </Badge>
+            </IconButton>
+          </div>
+        </ToolBar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default NavBar;
